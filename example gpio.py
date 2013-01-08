4e5038507c5fd707111298c58a22907da4b69097
JP Meijers <git@jpmeijers.com>
2013-01-08 15:55:46 +0200

    Added conf files for soundmodem and ax ports working gpio example more complete picontroller conf file picontroller main code started and receive fine

--- example gpio.py
+++ example gpio.py
@@ -1,7 +1,10 @@
 import RPi.GPIO as GPIO
+import time
 
 # to use Raspberry Pi board pin numbers
 GPIO.setmode(GPIO.BOARD)
+
+GPIO.cleanup()
 
 # set up GPIO output channel
 GPIO.setup(12, GPIO.OUT)
@@ -11,32 +14,40 @@ GPIO.output(12, GPIO.HIGH)
 
 # set up GPIO input with pull-up control
 #   (pull_up_down be PUD_OFF, PUD_UP or PUD_DOWN, default PUD_OFF)
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_OFF)
 
 # input from RPi board pin 11
 input_value = GPIO.input(11)
 
 # set up rising edge detection (EXPERIMENTAL)
-set_rising_event(11)
+GPIO.set_rising_event(11)
 
-# check for an event (EXPERIMENTAL)
-if GPIO.event_detected(11):
-    print('Rising edge detected!')
+#print "Waiting for an event"
 
 # set up falling edge detection (EXPERIMENTAL)
-GPIO.set_rising_event(11, enable=False)  # disable rising edge detection (as set above)
-GPIO.set_falling_event(11)
+#GPIO.set_rising_event(11, enable=False)  # disable rising edge detection (as set above)
+#GPIO.set_falling_event(11)
 
 # set up high detection (EXPERIMENTAL)
-GPIO.set_falling_event(11, enable=False)  # disable falling edge detection (as set above)
-GPIO.set_high_event(11)
+#GPIO.set_falling_event(11, enable=False)  # disable falling edge detection (as set above)
+#GPIO.set_high_event(11)
 
 # set up low detection (EXPERIMENTAL)
-GPIO.set_high_event(11, enable=False)  # disable high detection (as set above)
-GPIO.set_low_event(11)
+#GPIO.set_high_event(11, enable=False)  # disable high detection (as set above)
+#GPIO.set_low_event(11)
+
+# check for an event (EXPERIMENTAL)
+#if GPIO.event_detected(11):
+#    print('Rising edge detected!')
 
 # to change to BCM GPIO numbering
-GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
+
+while True:
+	if input_value != GPIO.input(11):
+		print "Pin 11 state changed."
+		input_value = GPIO.input(11)
+	time.sleep(1)
 
 # to reset every channel that has been set up by this program to INPUT with no pullup/pulldown and no event detection.
 GPIO.cleanup()
---
+++ picontroller.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+import ConfigParser
+import signal
+import subprocess
+import shutil
+import sys
+
+#global variables
+sm_process = None
+axlisten_process = None
+
+
+#load config file
+config = ConfigParser.ConfigParser()
+config.read(['picontroller.conf'])
+
+#Program exit
+def exit_handler(signal, frame):
+	global sm_process
+	global axlisten_process
+	
+	print '\nCtrl+C pressed, system exiting.'
+	#close all running threads and open files
+	
+	sm_process.terminate()
+	axlisten_process.terminate()
+	
+	sys.exit()
+signal.signal(signal.SIGINT, exit_handler)
+
+
+def write_sm_ax_configs():
+	shutil.copyfile('./soundmodem.conf', '/etc/ax25/soundmodem.conf')
+	shutil.copyfile('./axports', '/etc/ax25/axports')
+
+
+def start_soundmodem():
+	global sm_process
+	sm_process = subprocess.Popen(["soundmodem"])
+
+def start_axlisten():
+	global axlisten_process
+	axlisten_process = subprocess.Popen(["axlisten",config.get('soundmodem','sm_interface')], stdout=subprocess.PIPE)
+	
+	#read axlisten's output indefinitely
+	header = ""
+	data_buffer = ""
+	packet_length = 0
+	while True:
+		data = axlisten_process.stdout.readline()
+		
+		if (data.find(config.get('soundmodem','sm_interface')+": ") == 0):
+			header = data.lstrip(config.get('soundmodem','sm_interface')+": ")
+			position = header.rfind("len ")
+			packet_length = int(header[(position+4):].strip())
+			print "\nNew packet of length %d" % packet_length
+		
+		elif (packet_length != 0):
+			data_buffer += data[6:].strip("\n")
+			#print data
+			if(len(data_buffer) >= packet_length):
+				print "Received complete packet:"
+				print data_buffer
+				#do something with the received data
+				
+				#Now clear the buffers
+				header = ""
+				data_buffer = ""
+				packet_length = 0
+				
+
+
+#Entry point
+if __name__ == "__main__":
+	write_sm_ax_configs()
+	start_soundmodem()
+	start_axlisten()
+	
+	exit_handler(Null)
